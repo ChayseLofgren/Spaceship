75892a1f8da3aaffcd760648c2fb5e8da637e8f2
ChayseLofgren <cdl1643@g.rit.edu>
2021-10-08 14:27:37 -0400

    Added objects file, character class, and o0ne character

--- Main/src/objects/characters.java
+++ Main/src/objects/characters.java
@@ -1,5 +1,10 @@
 package objects;
 
 public class characters {
-    
+    class ArrakisKuma{
+        String Name="Arrakis Kuma";
+        String Backstory="Department Head of Plant Biology. They are a Lialspel botanist with a mind full of plants and a heart full of adventure. Though they can be serious at times, they certainly know when and how to cut loose, and which plants can help with it.";
+        String IconPath="";
+    }
+
 }
