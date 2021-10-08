74928032fe72e6f9ef52c5b2a2cbb75a32b48439
ChayseLofgren <cdl1643@g.rit.edu>
2021-10-08 14:50:02 -0400

    added bas4e character code

--- Main/src/objects/characters.java
+++ Main/src/objects/characters.java
@@ -1,10 +1,46 @@
 package objects;
 
 public class characters {
-    class ArrakisKuma{
+    class Arrakis_Kuma{
         String Name="Arrakis Kuma";
         String Backstory="Department Head of Plant Biology. They are a Lialspel botanist with a mind full of plants and a heart full of adventure. Though they can be serious at times, they certainly know when and how to cut loose, and which plants can help with it.";
+        String Pronouns="He|Him|His";
         String IconPath="";
     }
-
+    class Regulus_Agol{
+        String Name="Regulus Agol";
+        String Backstory="Department Head of Animal Biology. He discovered that the Yellowfin’s Automatic Translator™ has the capabilities to translate animal “language” which has opened new doors in animal behavioral study.";
+        String Pronouns="He|Him|His";
+        String IconPath="";
+    }
+    class Arcturus_Murphid{
+        String Name="Arcturus Murphid";
+        String Backstory="Department head of Operations and Tactical. He is a large, intimidating, bear of a human man who quietly guards the OCS Bouman and her operations.";
+        String Pronouns="He|Him|His";
+        String IconPath="";
+    }
+    class Nashira_Gamesia{
+        String Name="Nashira Gamesia";
+        String Backstory="Department Head of Technology. She keeps any information discovered by the Bouman crew up to date and sent back to Oceanic Corp. while making sure robotics and everything else on the ship runs smoothly.";
+        String Pronouns="She|Her|Hers";
+        String IconPath="";
+    }
+    class Tsze_Tseang{
+        String Name="Tsze Tseang";
+        String Backstory="";
+        String Pronouns="He|Him|His";
+        String IconPath="Department Head of Engineering. While he is very intelligent and knowledgeable about ships and their inner workings down to the manufacturing of every nut and bolt, he knows very little about how to properly socialize with other people. Apparently the grease and grime is unacceptable.";
+    }
+    class Dr_Yed_Prior{
+        String Name="Dr. Yed Prior";
+        String Backstory="Department Head of Medical. The Bouman’s surgeon is a Lialspel who has traveled the galaxy twice over and shows the wear with a ‘take no shit, but do no harm’ attitude. They are very fond of human alcoholic beverages.";
+        String Pronouns="He|Him|His";
+        String IconPath="";
+    }
+    class Sadal_Meliki{
+        String Name="Sadal Meliki";
+        String Backstory="Captain of the OCS Bouman. An older Lialspel who has travelled the galaxy with their old friend, Yed Prior. Grizzled and tough on the outside, they are oddly parental towards their crew.";
+        String Pronouns="He|Him|His";
+        String IconPath="";
+    }
 }
